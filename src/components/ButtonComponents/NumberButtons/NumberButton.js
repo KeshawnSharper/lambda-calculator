1272d638cb7bbb603a4a64acb75b06310497c066
unknown <ksharper@studentmba.org>
2019-09-11 08:56:11 -0400

    keshawnSharper

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Display } from "../../DisplayComponents/Display"
 
-const NumberButton = () => {
+
+export const NumberButton = (props) => {
+  const { number }  = props
+  let clickNumber =  () => <Display display={ number }/>
+      
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+  {<button onClick={ clickNumber }>{ number }</button>}
     </>
   );
 };
+
+
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { NumberButton } from "../ButtonComponents/NumberButtons/NumberButton"
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+export const Display = (props) => {
+  const { display } = props
+  
+  return <div>{ display }</div>
 };
